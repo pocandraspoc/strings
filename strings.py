1b2213a2a3278729a54fb6afbcf454da120f67a0
pocandraspoc <pocandraspoc@gmail.com>
2019-05-17 15:44:09 +0200

    init

---
+++ strings.py
@@ -0,0 +1,17 @@
+import unittest
+
+class test_consonant(unittest.TestCase):
+	"""docstring for test_consonant"""
+	def test_upper(self):
+		self.assertEqual('FOO'.upper(), 'FOO')
+
+	def test_consonant(self):
+		self.assertEqual(consonant('asztal'), 3)
+
+def consonant(string: str) ->int:
+	"""
+	"""
+	return 3
+
+if __name__ == '__main__':
+    unittest.main()
